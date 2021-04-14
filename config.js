9548d97ce18234533c194d397c601a94cfa28b8d
aingang028 <82096519+aingang028@users.noreply.github.com>
2021-04-14 15:28:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,15 @@
-global.owner = ['6289508963104','6281515860089'] // Put your number here
-global.mods = [] // Want some help?
+global.owner = ['6289508963104','6281515860089'] // Put your number hereglobal.mods = [] // Want some help?
+
 global.prems = [] // Premium user has unlimited limit
+
 global.APIs = { // API Prefix
+
   // name: 'https://website'
+
   nrtm: 'https://nurutomo.herokuapp.com',
+
   xteam: 'https://api.xteam.xyz'
+
 }
 
 global.APIKeys = { // APIKey Here
@@ -13,30 +18,30 @@ global.APIKeys = { // APIKey Here
 
   'https://api.xteam.xyz': 'd90a9e986e18778b'
 
-
-
-
-
-global.APIKeys = { // APIKey Here
-
-  // 'https://website': 'apikey'
-
-  'https://api.xteam.xyz': '833f655ae5e773e9'
-
 }
 
 // Sticker WM
-global.packname = 'Semoga Bahagia :)'
+
+global.packname = 'Semoga Bahagia'
+
 global.author = 'Binaldari'
 
 global.multiplier = 69 // The higher, The harder levelup
 
 let fs = require('fs')
+
 let chalk = require('chalk')
+
 let file = require.resolve(__filename)
+
 fs.watchFile(file, () => {
+
   fs.unwatchFile(file)
+
   console.log(chalk.redBright("Update 'config.js'"))
+
   delete require.cache[file]
+
   require(file)
+
 })
